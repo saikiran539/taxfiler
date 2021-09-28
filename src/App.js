a7e297cb396617a553e4431784790779be5fe117
saikiran539 <saikiran.chowdary1994@gmail.com>
2021-09-28 21:37:35 +0530

    footer_Final

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import NavMenu from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ReferBar from "./components/ReferBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -9,8 +10,10 @@ function App() {
       <Router>
         {/* <ReferBar /> */}
         <NavMenu />
+        <Footer />
         <Switch>
           <Route path="/" exact />
+          {/* <Route path="/Contact" component={Footer} /> */}
         </Switch>
       </Router>
     </>
